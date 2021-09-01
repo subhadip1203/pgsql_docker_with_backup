8bcf7458715be5c1ccb1e672faa6f935f2344f31
subhadip1203 <subhadip1203@gmail.com>
2021-09-01 23:21:25 +0530

    pgsql inside ubuntu

--- failed1/DB_backup/start.sh
+++ failed1/DB_backup/start.sh
@@ -6,14 +6,16 @@ db_user="$(printenv | grep -E "^POSTGRES_USER" | cut -d "=" -f 2)"
 db_password="$(printenv | grep -E "^POSTGRES_PASSWORD" | cut -d "=" -f 2)"
 db_name="$(printenv | grep -E "^POSTGRES_DB" | cut -d "=" -f 2)"
 
-echo "" >> /etc/cron.d/crontab2
-echo "export PGPASSWORD=${db_password}" >> /etc/cron.d/crontab2
+echo "" >> /myFolder/dbBackup_part2.sh
+echo "export PGPASSWORD=${db_password}" >> /myFolder/dbBackup_part2.sh
+echo "export PGPASSWORD=${db_password}" >> /myFolder/dbBackup_part2.sh
 
+pg_dump name_of_database > name_of_backup_file
+
+### cron jobs 
 printenv | grep -E "^POSTGRES_" > /myFolder/project_env.sh 
-
 cat /myFolder/project_env.sh  /myFolder/crontab > /etc/cron.d/crontab
 
-# cat /myFolder/crontab >> /etc/cron.d/crontab
 
 chmod 0644 /etc/cron.d/crontab 
 /usr/bin/crontab /etc/cron.d/crontab 
--- new1/start.sh
+++ new1/start.sh
@@ -7,9 +7,17 @@ db_password="$(printenv | grep -E "^POSTGRES_PASSWORD" | cut -d "=" -f 2)"
 db_name="$(printenv | grep -E "^POSTGRES_DB" | cut -d "=" -f 2)"
 
 psql -V
-
-psql --command "CREATE USER ${db_user} WITH SUPERUSER PASSWORD '${db_password}';"
-
-createdb -O ${db_user} ${db_name}
-
 /etc/init.d/postgresql start
+
+# creating new user and its password
+psql --command "CREATE USER ${db_user} WITH ENCRYPTED PASSWORD '${db_password}';"
+
+# creating new database for the user
+psql --command "create database ${db_name};"
+psql --command "grant all privileges on database ${db_name} to ${db_user};"
+
+/etc/init.d/postgresql stop
+
+exec "$@"
+
+
